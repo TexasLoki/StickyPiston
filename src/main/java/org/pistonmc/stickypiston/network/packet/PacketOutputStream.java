8e9b970b4b55566aa99a520f20f4c0ed8b5c412e
Harry Fox <harry.fox.ic@icloud.com>
2014-08-23 12:24:28 +0100

    Code Reformat

--- src/main/java/org/pistonmc/stickypiston/network/packet/PacketOutputStream.java
+++ src/main/java/org/pistonmc/stickypiston/network/packet/PacketOutputStream.java
@@ -6,39 +6,39 @@ import java.io.OutputStream;
 
 public class PacketOutputStream extends DataOutputStream {
 
-	public PacketOutputStream(OutputStream out) {
-		super(out);
-	}
+    public PacketOutputStream(OutputStream out) {
+        super(out);
+    }
 
-	public void writeString(String string) throws IOException {
-		writeVarInt(string.length());
-		write(string.getBytes("UTF-8"));
-	}
+    public void writeString(String string) throws IOException {
+        writeVarInt(string.length());
+        write(string.getBytes("UTF-8"));
+    }
 
-	public void writeVarInt(int paramInt) throws IOException {
-		while(true) {
-			if((paramInt & 0xFFFFFF80) == 0) {
-				writeByte((byte) paramInt);
-				return;
-			}
+    public void writeVarInt(int paramInt) throws IOException {
+        while(true) {
+            if((paramInt & 0xFFFFFF80) == 0) {
+                writeByte((byte) paramInt);
+                return;
+            }
 
-			writeByte((byte) (paramInt & 0x7F | 0x80));
-			paramInt >>>= 7;
-		}
-	}
+            writeByte((byte) (paramInt & 0x7F | 0x80));
+            paramInt >>>= 7;
+        }
+    }
 
-	public void writeVarInt64(long varInt) throws IOException {
-		int length = 10;
-		for(int i = 9; i >= 0; i--)
-			if(((varInt >> (i * 7)) & (i != 9 ? 0x7F : 0x01)) == 0)
-				length--;
-		for(int i = 0; i < length; i++)
-			writeByte((int) ((i == length - 1 ? 0x00 : 0x80) | ((varInt >> (i * 7)) & (i != 9 ? 0x7F : 0x01))));
-	}
+    public void writeVarInt64(long varInt) throws IOException {
+        int length = 10;
+        for(int i = 9; i >= 0; i--)
+            if(((varInt >> (i * 7)) & (i != 9 ? 0x7F : 0x01)) == 0)
+                length--;
+        for(int i = 0; i < length; i++)
+            writeByte((int) ((i == length - 1 ? 0x00 : 0x80) | ((varInt >> (i * 7)) & (i != 9 ? 0x7F : 0x01))));
+    }
 
-	public void writeByteArray(byte[] data) throws IOException {
-		writeShort(data.length);
-		write(data);
-	}
+    public void writeByteArray(byte[] data) throws IOException {
+        writeShort(data.length);
+        write(data);
+    }
 
 }
