7af6fb1702b025576ddffdd12ec8df4648344530
Harry Fox <harry.fox.ic@icloud.com>
2014-08-23 18:38:09 +0100

    Appropriate changes in StickyServer

--- src/main/java/org/pistonmc/stickypiston/StickyServer.java
+++ src/main/java/org/pistonmc/stickypiston/StickyServer.java
@@ -5,8 +5,11 @@ import org.pistonmc.Piston;
 import org.pistonmc.Server;
 import org.pistonmc.logging.Logger;
 import org.pistonmc.logging.Logging;
+import org.pistonmc.plugin.JavaPlugin;
+import org.pistonmc.plugin.JavaPluginManager;
 import org.pistonmc.plugin.protocol.ProtocolManager;
 import org.pistonmc.plugin.PluginManager;
+import org.pistonmc.stickypiston.network.NetworkServer;
 import org.pistonmc.util.reflection.SimpleObject;
 
 import java.io.File;
@@ -15,16 +18,18 @@ public class StickyServer implements Server {
 
     private Logger logger;
     private ProtocolManager protocols;
-    private PluginManager plugins;
+    private JavaPluginManager plugins;
+    private NetworkServer network;
 
     public StickyServer(OptionSet options) {
         new SimpleObject(null, Piston.class).field("server").set(this);
         this.logger = Logging.getLogger();
         this.protocols = new ProtocolManager(Logging.getLogger("Protocol", logger), (File) options.valueOf("protocols-folder"));
-        this.plugins = new PluginManager(logger, (File) options.valueOf("plugins-folder"), "plugin.json");
+        this.plugins = new JavaPluginManager(logger, (File) options.valueOf("plugins-folder"));
 
         this.protocols.reload(false);
         this.plugins.reload(false);
+        this.network = new NetworkServer(null); // add ip address here
     }
 
     public Logger getLogger() {
@@ -41,7 +46,9 @@ public class StickyServer implements Server {
 
     @Override
     public void shutdown() {
-        // shutdown the server
+        for(JavaPlugin plugin : plugins.getPlugins()) {
+            plugin.setEnabled(false);
+        }
     }
 
 }
