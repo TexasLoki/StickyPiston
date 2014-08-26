905c5b33d329dbad954a558d6fac1db8282e30a8
Harry Fox <harry.fox.ic@icloud.com>
2014-08-26 14:33:06 +0100

    Use PlayerConnectionHandler as the Handler

--- src/main/java/org/pistonmc/stickypiston/network/StickyChannelInitializer.java
+++ src/main/java/org/pistonmc/stickypiston/network/StickyChannelInitializer.java
@@ -2,12 +2,13 @@ package org.pistonmc.stickypiston.network;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import org.pistonmc.stickypiston.network.player.PlayerConnectionHandler;
 
 public class StickyChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     public void initChannel(SocketChannel ch) throws Exception {
-        ch.pipeline().addLast(new PacketDecoder());
+        ch.pipeline().addLast(new PacketDecoder(), new PlayerConnectionHandler());
     }
 
 }
